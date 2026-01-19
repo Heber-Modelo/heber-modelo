82f0d2140e2e0526f5e879bacc87273327b9bbfc
Heber F Barra <heberferreirabarra@gmail.com>
2026-01-19 15:24:46 -0300

    [ ENHANCEMENT ] Troca de pre-commit.py por um arquivo pre-commit-config para simplicar o processo de criação de hooks

--- gruntfile.js
+++ gruntfile.js
@@ -1,24 +1,24 @@
-// https://stackoverflow.com/questions/17008472/how-to-minify-multiple-javascript-files-in-a-folder-with-uglifyjs
+// BOROWY, Dziad. How to minify multiple Javascript files in a folder with UglifyJS.
+// Postado em: 9 jun. 2013 [site]. Disponível em: <https://stackoverflow.com/questions/17008472>.
+// Acesso em: 25 set. 2024.
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  grunt.initConfig({
+    uglify: {
+      files: {
+        src: "src/main/resources/static/javascript/*.js",
+        dest: "src/main/resources/static/javascript",
+        expand: true,
+        flatten: true,
+        ext: ".js",
+      },
+    },
+    watch: {
+      js: { files: "src/main/resources/static/javascript/*.js", tasks: ["uglify"] },
+    },
+  });
 
-    grunt.initConfig({
-        uglify: {
-            files: {
-                src: "src/main/resources/static/javascript/*.js",
-                dest: "src/main/resources/static/javascript",
-                expand: true,
-                flatten: true,
-                ext: ".js"
-            }
-        },
-        watch: {
-            js: {files: "src/main/resources/static/javascript/*.js", tasks: [ "uglify" ]}
-        }
-    
-    });
-    
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.registerTask("default", ["uglify"]);
-}
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.registerTask("default", ["uglify"]);
+};
