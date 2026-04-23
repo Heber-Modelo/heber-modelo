6c1bc47d6b3bafe6ea714045f27ec3e7c77209ac
MatheusJ28 <20233026297@estudantes.ifpr.edu.br>
2026-04-22 22:02:24 -0300

    novidades em páginas de cadastro, login e sessão

---
+++ src/main/resources/static/typescript/application/paginas/editor/toolbar.ts
@@ -0,0 +1,43 @@
+import CopiarComponenteCommand, {
+  CopiarComponenteCommandBuilder,
+} from "infrastructure/command/copiarComponenteCommand";
+
+
+let buttonArquivo: HTMLDivElement | null = document.querySelector("button#arquivo");
+let buttonCopiar: HTMLDivElement | null = document.querySelector("button#copiar");
+let buttonColar: HTMLDivElement | null = document.querySelector("button#colar");
+let buttonCortar: HTMLDivElement | null = document.querySelector("button#cortar");
+let buttonRefazer: HTMLDivElement | null = document.querySelector("button#refazer");
+let buttonDesfazer: HTMLDivElement | null = document.querySelector("button#desfazer");
+let buttonApagar: HTMLDivElement | null = document.querySelector("button#apagar");
+let buttonDeletar: HTMLDivElement | null = document.querySelector("button#deletar");
+
+
+
+
+buttonDeletar?.addEventListener("click", ()=>{
+
+});
+
+buttonApagar?.addEventListener("click", ()=>{
+
+});
+
+buttonDesfazer?.addEventListener("click", ()=>{
+
+});
+
+buttonRefazer?.addEventListener("click", ()=>{
+
+});
+
+buttonCopiar?.addEventListener("click", ()=>{
+});
+
+buttonColar?.addEventListener("click", ()=>{
+
+});
+
+buttonCortar?.addEventListener("click", ()=>{
+
+});
