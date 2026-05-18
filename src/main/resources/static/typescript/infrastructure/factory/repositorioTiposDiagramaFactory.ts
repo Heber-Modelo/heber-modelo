0060ee382aea3679f3be298dbdf3626d2dd5c634
Heber F Barra <heberferreirabarra@gmail.com>
2026-05-18 15:46:24 -0300

    [ ENHANCEMENT ] O comando de carregar diagramas agora atualiza a listagem no corpo do main

---
+++ src/main/resources/static/typescript/infrastructure/factory/repositorioTiposDiagramaFactory.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2026. Heber Ferreira Barra, Matheus de Assis de Paula, Matheus Jun Alves Matuda.
+ *
+ * Licensed under the Massachusetts Institute of Technology (MIT) License.
+ * You may obtain a copy of the license at:
+ *
+ *    https://choosealicense.com/licenses/mit/
+ *
+ * A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ *
+ */
+
+import RepositorioTiposDiagrama from "infrastructure/repositorio/repositorioTiposDiagrama";
+
+export default class RepositorioTiposDiagramaFactory {
+  private static _instance: RepositorioTiposDiagrama | null = null;
+
+  public static build(): RepositorioTiposDiagrama {
+    if (this._instance === null) {
+      this._instance = new RepositorioTiposDiagrama();
+    }
+
+    return this._instance;
+  }
+
+}
