1df23fe46a4cca36bce8d42e08f7debab6b39d8c
Heber F Barra <heberferreirabarra@gmail.com>
2026-08-10 14:32:49 -0300

    [ FEATURE ] Ajuste na configuração do Spring Security para utilizar CSRF

--- src/main/java/io/github/heberbarra/modelador/infrastructure/security/ConfiguradorSecurity.java
+++ src/main/java/io/github/heberbarra/modelador/infrastructure/security/ConfiguradorSecurity.java
@@ -17,10 +17,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -33,8 +33,9 @@ public class ConfiguradorSecurity {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
-        // TODO: Configurar apropriadamente o token CSRF após configurar o sistema de contas
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.csrf(csrf -> {
+            csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        });
 
         httpSecurity.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/usuarios", "/usuarios/**")
