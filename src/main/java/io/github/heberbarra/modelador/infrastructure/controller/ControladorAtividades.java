4dfa765147c41cd70faa84b0929fbaceb5f3097e
MatheusJ28 <20233026297@estudantes.ifpr.edu.br>
2026-08-24 15:49:00 -0300

    inicio de implementação de criar atividade

---
+++ src/main/java/io/github/heberbarra/modelador/infrastructure/controller/ControladorAtividades.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026. Heber Ferreira Barra, Matheus de Assis de Paula, Matheus Jun Alves Matuda.
+ *
+ * Licensed under the Massachusetts Institute of Technology (MIT) License.
+ * You may obtain a copy of the license at:
+ *
+ *    https://choosealicense.com/licenses/mit/
+ *
+ * A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ *
+ */
+
+package io.github.heberbarra.modelador.infrastructure.controller;
+
+import io.github.heberbarra.modelador.ControladorWeb;
+import io.github.heberbarra.modelador.infrastructure.data.DataSourceBuilder;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControladorAtividades {
+
+    @PostMapping({"criarAtividade"})
+    public String criarAtividade() {
+
+        return "redirect:/listagemEstudantes";
+    }
+
+}
